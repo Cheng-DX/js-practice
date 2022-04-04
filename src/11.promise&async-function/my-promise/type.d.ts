0cc11754b0c34a10227546f6a277763b50c4ff2d
Cheng_DX <2337040896@qq.com>
2022-04-04 23:22:39 +0800

    fix: fix the param type of PromiseExecFn

--- src/11.promise&async-function/my-promise/type.d.ts
+++ src/11.promise&async-function/my-promise/type.d.ts
@@ -9,7 +9,7 @@ export interface MyPromiseType {
 export type ResolveFn = (value: unknown) => void
 export type RejectFn = (reason?: any) => void
 
-export type PromiseExecFn = (resolve: RejectFn, reject: RejectFn) => any
+export type PromiseExecFn = (resolve: ResolveFn, reject: RejectFn) => any
 
 export type onResolvedFn = (value: unknown) => any
 export type onRejectdFn = (reason: any) => any
