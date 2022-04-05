cb0f0bf41c4d11010ece21a33c0e5eafdd9d31e7
Cheng_DX <2337040896@qq.com>
2022-04-05 15:03:53 +0800

    style: the end of my-promise

--- src/11.promise&async-function/my-promise/index.ts
+++ src/11.promise&async-function/my-promise/index.ts
@@ -1,2 +1,14 @@
+/**
+ * 来自己写一个promise吧
+ * - 由于JavaScript接触不到微任务的分配,直接用宏任务模拟即可 checked
+ * - 包含三种状态且能够正确切换 checked
+ * - 支持多个回调函数的绑定 checked
+ * - then的返回值问题 checked
+ * - catch异常事件冒泡 checkd
+ * - Promise.resolve(),Promise.reject() checked
+ * - finally checked
+ * - Promise.all(),Promise.rece() checked
+ */
+
 export * from './promise'
-export * from './shared'
+export * from './type'
--- src/11.promise&async-function/my-promise/promise.ts
+++ src/11.promise&async-function/my-promise/promise.ts
@@ -1,14 +1,3 @@
-/**
- * 来自己写一个promise吧
- * - 由于JavaScript接触不到微任务的分配,直接用宏任务模拟即可 checked
- * - 包含三种状态且能够正确切换 checked
- * - 支持多个回调函数的绑定 checked
- * - then的返回值问题 checked
- * - catch异常事件冒泡 checkd
- * - Promise.resolve(),Promise.reject() checked
- * - finally checked
- * - Promise.all(),Promise.rece()
- */
 import { PENDING, FULFILLED, REJECTED } from './shared'
 import type {
   PromiseExecFn,
@@ -25,6 +14,7 @@ import { reject as staticRejectFn } from './static-functions/promise-reject'
 import { myFinally } from './handler/finally'
 import { all as staticAllFn } from './static-functions/promise-all'
 import { race as staticRaceFn } from './static-functions/promise-race'
+
 function MyPromise(execFn: PromiseExecFn) {
   // MyPromise instance
   const self: MyPromiseType = this
