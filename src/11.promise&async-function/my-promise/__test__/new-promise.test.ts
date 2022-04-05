997f77dd38136f8e7073b46bf559baa6bb25e1c2
Cheng_DX <2337040896@qq.com>
2022-04-05 13:13:12 +0800

    test: test the clain then call

--- src/11.promise&async-function/my-promise/__test__/new-promise.test.ts
+++ src/11.promise&async-function/my-promise/__test__/new-promise.test.ts
@@ -4,24 +4,14 @@ import { log } from '../shared'
 Array.from({ length: 2 }, (_, index) => index).forEach(idx => {
   let mp = new MyPromise((resolve, reject) => {
     if (idx === 0) {
-      setTimeout(() => resolve(`${idx} resolved`), 1000)
+      resolve(`${idx} resolved`)
     } else {
       reject(`${idx} rejected`)
     }
   })
-  mp.then((value: any) => {
-    console.log(`MyPromise ${idx} in then 1: ${value}`)
-  })
-
-  mp.then((value: any) => {
-    console.log(`MyPromise ${idx} in then 2: ${value}`)
-  })
-
   mp.catch((reason: any) => {
     console.error(`MyPromise ${idx} in catch 1: ${reason}`)
-  })
-
-  mp.catch((reason: any) => {
+  }).catch((reason: any) => {
     console.error(`MyPromise ${idx} in catch 2: ${reason}`)
   })
   log(mp)
