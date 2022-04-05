9f4cc3c0d8fa347024827783ec1e91575e0c3d2b
Cheng_DX <2337040896@qq.com>
2022-04-05 12:43:04 +0800

    feat: then return

--- src/11.promise&async-function/my-promise/handler/then.ts
+++ src/11.promise&async-function/my-promise/handler/then.ts
@@ -1,10 +1,87 @@
-import { MyPromiseType, onRejectdFn, onResolvedFn } from '../type'
+import { MyPromise } from '../promise'
+import { FULFILLED, PENDING, REJECTED, nop } from '../shared'
+import {
+  MyPromiseType,
+  onRejectdFn,
+  onResolvedFn,
+  RejectFn,
+  ResolveFn
+} from '../type'
 
 export function then(
-  onResolvedCallbackFn: onResolvedFn,
-  onRejectdCallbackFn: onRejectdFn
+  onResolvedCallbackFn: onResolvedFn = nop,
+  onRejectdCallbackFn: onRejectdFn = nop
 ) {
   const instance: MyPromiseType = this
-  if (onResolvedCallbackFn) instance.onResolvedFnList.push(onResolvedCallbackFn)
-  if (onRejectdCallbackFn) instance.onRejectdFnList.push(onRejectdCallbackFn)
+  let thenPromise = null
+  if (instance.status === PENDING) {
+    // return a new MyPromise instanc
+    thenPromise = new MyPromise((resolve, reject) => {
+      instance.onResolvedFnList.push(value => {
+        try {
+          const result = onResolvedCallbackFn(value)
+          unwrapResult(instance, result, resolve, reject)
+        } catch (e) {
+          reject(e)
+        }
+      })
+      instance.onResolvedFnList.push(reason => {
+        try {
+          const result = onRejectdCallbackFn(reason)
+          unwrapResult(instance, result, resolve, reject)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+  } else if (instance.status === FULFILLED) {
+    thenPromise = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          const result = onResolvedCallbackFn(instance.value)
+          unwrapResult(instance, result, resolve, reject)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+  } else if (instance.status === REJECTED) {
+    thenPromise = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          const result = onRejectdCallbackFn(instance.reason)
+          unwrapResult(instance, result, resolve, reject)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+  } else {
+    throw new Error('status error')
+  }
+
+  return thenPromise
+}
+
+function unwrapResult(
+  instance: MyPromiseType,
+  result: any,
+  resolve: ResolveFn,
+  reject: RejectFn
+) {
+  if ((result as MyPromiseType) instanceof MyPromise) {
+    if (result.status === PENDING) {
+      result
+        .then((y: any) => {
+          unwrapResult(instance, y, resolve, reject)
+        })
+        .catch((err: any) => {
+          reject(err)
+        })
+    } else {
+      result.then(resolve, reject)
+    }
+  } else {
+    resolve(result)
+  }
 }
