32128c3e7eec52d6dbc48a00a72a7c1df137e378
Cheng_DX <2337040896@qq.com>
2022-04-06 22:48:22 +0800

    学了三年动画做出来的

---
+++ src/other/css/transform.ts
@@ -0,0 +1,14 @@
+const root = document.querySelector('#root')
+const styleDiv = document.querySelector('.style')
+
+const style = `<style>
+p{
+}
+#root: hover{
+  color: #fff;
+  transform: rotateZ(180deg);
+  background-color: red;
+}
+</style>`
+
+// styleDiv.innerHTML = style
