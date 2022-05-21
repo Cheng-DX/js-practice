7fd581dcc6fa171b578475d5fe2e5540a9df1e69
Cheng_DX <2337040896@qq.com>
2022-05-21 21:31:59 +0800

    feat: many utks

--- src/typescript/util.ts
+++ src/typescript/util.ts
@@ -1,45 +1,126 @@
-export type Pick<T, U extends keyof T> = { [P in U]: T[P] }
-
-export type Omit<T, U extends keyof T> = {
-  [P in Exclude<keyof T, U>]: T[P]
+type Src = {
+  a: string
+  b: number
+  c?: boolean
+  d: symbol
+  e: null
+  f: undefined
+  g: object
 }
 
+/**
+ * Make each parameter optional
+ */
+export type Partial<T> = {
+  [P in keyof T]?: T[P]
+}
+type PartialA = Partial<Src>
+
+/**
+ * Make each parameter readonly
+ */
 export type Readonly<T> = {
   readonly [P in keyof T]: T[P]
 }
+type ReadonlyA = Readonly<Src>
 
-export type Union<T, U> = {
-  [P in keyof T]: T[P]
-} & {
-  [P in keyof U]: U[P]
+/**
+ * Create a new type with K:T
+ */
+export type Record<K extends string | number | symbol, T> = {
+  [P in K]: T
 }
+type RecordA = Record<'home' | 1, string>
 
-export type Readonlysome<T, U extends keyof T> = Union<
-  {
-    readonly [P in U]: T[P]
-  },
-  Omit<T, U>
->
+/**
+ * Create new type by picking parameter(s) from T
+ */
+export type Pick<T, U extends keyof T> = { [P in U]: T[P] }
+type PickA = Pick<Src, 'a' | 'b'>
+type PickB = Pick<Src, 'a'>
 
-type Src = {
-  a: number
-  b: string
-  c: boolean
+/**
+ * Create new type by omitting parameter(s) from T
+ */
+export type Omit<T, U extends keyof T> = {
+  [P in Exclude<keyof T, U>]: T[P]
 }
+type OmitA = Omit<Src, 'a' | 'b'>
+type OmitB = Omit<Src, 'a'>
 
-type A = Readonlysome<Src, 'a' | 'b'>
+/**
+ * Create a new type by removing types extending U in T
+ */
+export type Exclude<T, U> = T extends U ? never : T
+type ExcludeA = Exclude<Src | 'a', Src>
 
-declare function f<T extends boolean>(x: T): T extends true ? string : number
+/**
+ * Create a new type by picking types extending U in T
+ */
+export type Extract<T, U> = T extends U ? T : never
+type ExtractA = Extract<'a', Src>
 
-// Type is 'string | number
-let x = f(true)
+/**
+ * Create a new type by removing null and undefined from T
+ */
+export type NonNullable<T> = T extends null | undefined ? never : T
+type NonNullableA = NonNullable<string | number | undefined>
 
-type ReturnType1<T extends Function> = T extends (...args: any[]) => infer R
+/**
+ * Prameter type of function
+ */
+export type Parameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer P
+) => any
+  ? P
+  : never
+type ParametersA = Parameters<(a: string, b: bigint) => void>
+
+/**
+ * Prameter type of construtor function
+ */
+export type ConstructorParameters<T extends new (...args: any[]) => any> =
+  T extends new (...args: infer P) => any ? P : never
+type ConstructorParametersA = ConstructorParameters<FunctionConstructor>
+
+/**
+ * Return type of function
+ */
+export type ReturnType<T extends (...args: any[]) => any> = T extends (
+  ...args: any[]
+) => infer R
   ? R
   : any
+type ReturnTypeA = ReturnType<() => string>
 
-function A() {
-  console.log('1')
+/**
+ * Create a new type from a constructor's instance
+ */
+export type InstanceType<T extends new (...args: any[]) => any> =
+  T extends new (...args: any[]) => infer R
+    ? {
+        [K in keyof R]: R[K]
+      }
+    : any
+
+type InstanceTypeA = InstanceType<PromiseConstructor>
+
+/**
+ * Make each parameter required
+ */
+export type Required<T> = {
+  [K in keyof T]-?: T[K]
 }
+type RequiredA = Required<Src>
 
-type Re = ReturnType1<typeof A>
+/**
+ * This type of funtion
+ */
+export type ThisParameterType<T extends (this: any, ...args: any[]) => any> =
+  T extends (this: infer ThisType, ...args: any[]) => any ? ThisType : unknown
+function toHex(this: number) {
+  return this.toString(16)
+}
+function numberToString(n: ThisParameterType<typeof toHex>) {
+  return toHex.apply(n)
+}
